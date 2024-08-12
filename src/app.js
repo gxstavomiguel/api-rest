3c886cc2dc52e915fcc796c2c2decc1f2b138720
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-12 19:29:38 -0300

    Nova rotas criadas para acessar via id

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 import express from "express"; //const express = require('express') método otimizado para poder ser usado em outros arquivos
 const app = express();
 
-
 //para o express ler body.json
-app.use(express.json())
+app.use(express.json());
 
 // mock
 const selecoes = [
-    {id: 1, selecao: 'Brasil', grupo: 'G'},
-    {id: 2, selecao: 'Suíça', grupo: 'G'},
-    {id: 3, selecao: 'Sérvia', grupo: 'G'},
-    {id: 4, selecao: 'Camarões', grupo: 'G'}
-]
+  { id: 1, selecao: "Brasil", grupo: "G" },
+  { id: 2, selecao: "Suíça", grupo: "G" },
+  { id: 3, selecao: "Camarões", grupo: "G" },
+  { id: 4, selecao: "Sérvia", grupo: "G" },
+];
+
+//retornar o objeto por id
+function buscarSelecaoPorId(id) {
+  return selecoes.filter((selecao) => selecao.id == id);
+}
+
+//pegar a posição do elemento dentro do array via id
+function buscarIndexSelecao(id){
+    return selecoes.findIndex( selecao => selecao.id == id )
+}
 
 // rota padrão
 app.get("/", (req, res) => {
@@ -19,12 +28,19 @@ app.get("/", (req, res) => {
 });
 
 app.get("/selecoes", (req, res) => {
-    res.status(200).send(selecoes)
-})
+  res.status(200).send(selecoes);
+});
 
-app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body)
-    res.status(201).send('Seleção cadastrada com sucesso!')
-})
+// get de selecoes por id
+app.get("/selecoes/:id", (req, res) => {
+  res.json(buscarSelecaoPorId(req.params.id));
+});
+
+app.post("/selecoes", (req, res) => {
+  selecoes.push(req.body);
+  res.status(201).send("Seleção cadastrada com sucesso!");
+});
+
+app.delete()
 
 export default app;
