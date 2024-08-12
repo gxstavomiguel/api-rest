4197eb1971852f4260eda145ca8b8ade755bd86a
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-12 19:47:52 -0300

    Código refatorado ainda não consegue conectar com o método para atualizar

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express"; //const express = require('express') método otimizado para poder ser usado em outros arquivos
 const app = express();
 
-//para o express ler body.json
+// para o express ler body.json
 app.use(express.json());
 
 // mock
@@ -12,14 +12,14 @@ const selecoes = [
   { id: 4, selecao: "Sérvia", grupo: "G" },
 ];
 
-//retornar o objeto por id
+// retornar o objeto por id
 function buscarSelecaoPorId(id) {
   return selecoes.filter((selecao) => selecao.id == id);
 }
 
-//pegar a posição do elemento dentro do array via id
-function buscarIndexSelecao(id){
-    return selecoes.findIndex( selecao => selecao.id == id )
+// pegar a posição do elemento dentro do array via id
+function buscarIndexSelecao(id) {
+  return selecoes.findIndex((selecao) => selecao.id == id);
 }
 
 // rota padrão
@@ -41,10 +41,28 @@ app.post("/selecoes", (req, res) => {
   res.status(201).send("Seleção cadastrada com sucesso!");
 });
 
-app.delete('selecoes/:id', (req, res) => {
-    let index = buscarIndexSelecao(req.params.id)
-    selecoes.splice(index, 1)
-    res.send(`Seleção com id ${req.params.id} excluída com sucesso!`)
-})
+// Corrigindo a rota DELETE
+app.delete("/selecoes/:id", (req, res) => {
+  let index = buscarIndexSelecao(req.params.id);
+  if (index !== -1) {
+    selecoes.splice(index, 1);
+    res.send(`Seleção com id ${req.params.id} excluída com sucesso!`);
+  } else {
+    res.status(404).send("Seleção não encontrada");
+  }
+});
+
+// Corrigindo a rota PUT
+app.put("/selecoes/:id", (req, res) => {
+  let index = buscarIndexSelecao(req.params.id);
+  if (index !== -1) {
+    selecoes[index].selecao = req.body.selecao;
+    selecoes[index].grupo = req.body.grupo;
+    res.json(selecoes);
+  } else {
+    res.status(404).send("Seleção não encontrada");
+  }
+});
 
 export default app;
+
