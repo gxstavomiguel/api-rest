2d31197fd363e997a3c26671c8471d31d8564e30
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-11 21:36:35 -0300

    Configuração inicial do servidor

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// rota padrão
+app.get('/', (req, res) => {
+    res.send('Olá mundo!')
+})
+
+// escutando a porta 3000
+app.listen(port, () => {
+    console.log(`Server rodando no endereço http://localhost:${port}`)
+})
